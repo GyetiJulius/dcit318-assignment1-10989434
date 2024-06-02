45a1bd10856480dda921e9c2f9928ddda25f6fea
Julius Gyeti 10989434 <jgyeti@st.ug.edu.gh>
2024-06-02 09:12:48 -1200

    Created the GradeCalculator

--- Solutions/Solutions/Program.cs
+++ Solutions/Solutions/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    internal class Program
+    {
+        public void GradeCalculator()
+        {
+            Console.WriteLine("Enter your grade between 0 and 100");
+            int grade = int.Parse(Console.ReadLine());
+
+            string gradeLetter;
+
+            if (grade >= 90 && grade <= 100) gradeLetter = "A";
+            else if (grade >= 80  && grade <= 89) gradeLetter = "B";
+            else if (grade >= 70  && grade <= 79) gradeLetter = "C";
+            else if (grade >= 60  && grade <= 69) gradeLetter = "D";
+            else if (grade >= 0  && grade <= 59) gradeLetter = "F";
+            else{
+                Console.WriteLine("Invalid grade entered");
+                return;
+            }
+
+            Console.WriteLine($"Your grade is " + gradeLetter);
+            Console.ReadLine();
+
+        }
+    }
+}
